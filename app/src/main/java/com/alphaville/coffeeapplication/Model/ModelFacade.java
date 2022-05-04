5371457041f8d5107ae06dba4831b5203067364c
Valle20 <valdemar433@gmail.com>
2022-05-04 12:39:39 +0200

    Git stop resisting

--- app/src/main/java/com/alphaville/coffeeapplication/Model/ModelFacade.java
+++ app/src/main/java/com/alphaville/coffeeapplication/Model/ModelFacade.java
@@ -1,12 +1,14 @@
 package com.alphaville.coffeeapplication.Model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A facade-class for the applications model. It is used by the ViewModels to handle the model.
  */
 public class ModelFacade {
 
+    ReviewHandler reviewHandler = new ReviewHandler();
     CoffeeProduct active;
 
     /**
@@ -36,5 +38,14 @@ public class ModelFacade {
 
     public void setActive(CoffeeProduct active) {
         this.active = active;
+    }
+
+    public List<Review> searchInReviews(String s){
+        //pass on call to ReviewHandler
+        return new ArrayList<>();
+    }
+
+    public List<Review> getReviews(){
+        return reviewHandler.getReviews();
     }
 }
--- app/src/main/java/com/alphaville/coffeeapplication/views/HistoryFragment.java
+++ app/src/main/java/com/alphaville/coffeeapplication/views/HistoryFragment.java
@@ -4,19 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.alphaville.coffeeapplication.R;
-
-//Placeholder class, which will be replaced.
+import com.alphaville.coffeeapplication.databinding.FragmentHistoryBinding;
+import com.alphaville.coffeeapplication.viewModels.HistoryTabViewModel;
 
 public class HistoryFragment extends Fragment {
+
+    private FragmentHistoryBinding binding;
+    private HistoryTabViewModel model;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_history,container,false);
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        //binding.reviewList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        binding.reviewList.setAdapter(new HistoryResultAdapter(model.getReviews()));
+
+        /**
+         * Listener that should be triggered everytime the user changes anything in the search-field.
+         * This is if we want continuous updates while writing.
+         */
+        binding.searchHistory.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                binding.reviewList.setAdapter(new HistoryResultAdapter(model.getReviews()));
+                model.searchInReviews(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                binding.reviewList.setAdapter(new HistoryResultAdapter(model.getReviews()));
+                return true;
+            }
+        });
+        return view;
     }
 }
