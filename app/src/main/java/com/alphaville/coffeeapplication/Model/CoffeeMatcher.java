e1d24d4f4e9d8aab3cca8b478b9b6a5c3522cdf3
bomandavid <david.boman99@hotmail.com>
2022-05-10 18:14:20 +0200

    skapade Tuple klass samt sortByMatch

--- app/src/main/java/com/alphaville/coffeeapplication/Model/CoffeeMatcher.java
+++ app/src/main/java/com/alphaville/coffeeapplication/Model/CoffeeMatcher.java
@@ -1,10 +1,14 @@
 package com.alphaville.coffeeapplication.Model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class CoffeeMatcher {
 
+    //TODO: tester
     /** For each of sweetness, bitternss and fullness, the algorithm checks all previous review with
      * the same value and calculates the average rating for all those reviews. Then an average is takes for
      * all three parameters to ge the match score.
@@ -27,6 +31,24 @@ public class CoffeeMatcher {
         return (Mean(s_match) + Mean(b_match) + Mean(f_match))/3;
 
     }
+
+    //TODO: tester
+    /** Calculates all matchScores for alla coffeeProducts and sorts them based on highest match
+     * @param coffeeProducts List of all coffeProducts
+     * @param reviews List of all reviews
+     * @return sorted list of tuples with coffeProduct and matchScore
+     */
+    public static List<Tuple> sortByMatch(List<CoffeeProduct> coffeeProducts, List<Review> reviews) {
+        List<Tuple> res = new ArrayList<>();
+        for (CoffeeProduct c : coffeeProducts) {
+            res.add(new Tuple(c, getMatchPercentage(c, reviews)));
+        }
+
+        Collections.sort(res, Tuple::compareTo);
+
+        return res;
+    }
+
     /** Calculates the mean (average) value from a list of numeric values.
      * @param values List of numeric values
      * @return Mean (average) value
@@ -50,6 +72,20 @@ public class CoffeeMatcher {
             sum += value;
 
         return sum;
+    }
+
+    private static class Tuple implements Comparable<Tuple> {
+        private CoffeeProduct coffeeProduct;
+        private Double matchScore;
+
+        public Tuple(CoffeeProduct coffeeProduct, Double matchScore) {
+            this.coffeeProduct = coffeeProduct;
+            this.matchScore = matchScore;
+        }
+        @Override
+        public int compareTo(Tuple other) {
+            return other.matchScore.compareTo(matchScore); //rätt??
+        }
     }
 
 }
