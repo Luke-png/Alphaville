28f015012c5837a00747870c6c5a52940f8430c6
felixedholm <felix.edholm@gmail.com>
2022-05-10 09:53:16 +0200

    Added JavaDoc comments

--- app/src/main/java/com/alphaville/coffeeapplication/Model/ReviewDao.java
+++ app/src/main/java/com/alphaville/coffeeapplication/Model/ReviewDao.java
@@ -11,18 +11,38 @@ import androidx.room.Update;
 
 import java.util.List;
 
+/**
+ * A Data Access Object (DAO) interface for saving {@link Review} objects to database.
+ */
 @Dao
 public interface ReviewDao {
 
+    /**
+     * Inserts a review into the database
+     * @param review the review to insert into the database
+     */
     @Insert
     void insert(Review review);
 
+    /**
+     * Updates a review object if it exists in the database, based on primary key.
+     * If no object with that primary key exists, no changes are made to the database.
+     * @param review the review object to update
+     */
     @Update
     void update(Review review);
 
+    /**
+     * Deletes a review object from the database
+     * @param review the review object to delete
+     */
     @Delete
     void delete(Review review);
 
+    /**
+     * Returns an observable LiveData object containing all reviews in the database
+     * @return the observable LiveData object
+     */
     @Query("SELECT * FROM reviews")
     LiveData<List<Review>> getAllReviews();
 }
